0d9e23b3ccb3aa8206a068afe99d75708b57f940
Yingbo (Max) Wang <maxwyb@gmail.com>
2016-05-10 12:33:56 -0700

    Small bugs fixed. Add comments. Clean code.

--- AndroidData/app/src/main/java/com/ucla/max/androiddata/MainActivity.java
+++ AndroidData/app/src/main/java/com/ucla/max/androiddata/MainActivity.java
@@ -18,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
     public static int DATA_COUNT = 10;
     public static Integer[] num = new Integer[DATA_COUNT];
 
-    public static String textViewMessage; // the right-side TextView message for showing results
+//    public static String textViewMessage; // the right-side TextView message for showing results
 
     public static String temperature = ""; // a parsed String to send temperature data to server
     public static String result = ""; // the analysis result coming from server
 
-    public static String PC_IP = "131.179.30.188";
+    public static String PC_IP = "131.179.30.165";
     public static String ANDROID_IP = "131.179.45.16";
+    public static Integer PORT = 9930;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +46,26 @@ public class MainActivity extends AppCompatActivity {
             output += (num[i].toString() + ", ");
         }
         TextView textView1 = (TextView) findViewById(R.id.textView1);
-        textView1.setText(output);
+        try {
+            textView1.setText(output);
+        } catch (NullPointerException exception) {
+            Log.d("sunnyDay", exception.getMessage());
+        }
 
         sendDataToServer();
-        updateTextView2(result);
         receiveResultFromServer();
+
+        updateTextView2(result);
     }
+
 
     public static void updateTextView2(String str) {
         TextView textView2 = (TextView) instance.findViewById(R.id.textView2);
-        textView2.setText(str);
+        try {
+            textView2.setText(str);
+        } catch (NullPointerException exception) {
+            Log.d("sunnyDay", exception.getMessage());
+        }
     }
 
     public static void generateTemperature() {
@@ -76,20 +87,19 @@ public class MainActivity extends AppCompatActivity {
         }
         temperature += (num[DATA_COUNT - 1].toString() + "\n");
 
-
         Log.d("sunnyDay", temperature);
 
         // send this String to the server
         new Thread() {
             public void run() {
-
                 Socket mySocket = null;
                 DataOutputStream os = null;
                 // DataInputStream is = null;
                 BufferedReader is = null;
 
+                // initialize a Socket for TCP/IP communication; here Android device is client and PC is server
                 try {
-                    mySocket = new Socket("131.179.30.188", 9930);
+                    mySocket = new Socket(PC_IP, PORT);
                     os = new DataOutputStream(mySocket.getOutputStream());
                     // is = new DataInputStream(mySocket.getInputStream());
                     is = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
@@ -109,10 +119,9 @@ public class MainActivity extends AppCompatActivity {
 //                            os.writeInt(num[i]);
 //                        }
 
-                        String responseLine;
-
-                        // close the Socket in client mode; later start in server mode to receiver result data from server.
+                        // currently we are not expecting reply from the server
                         /*
+                        String responseLine;
                         Log.d("sunnyDay", "Waiting to get response from server...");
                         while ((responseLine = is.readLine()) != null) {
                             String message = "Got server reply: " + responseLine;
@@ -123,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                         */
 
+                        // close the Socket in client mode; later start in server mode to receiver result data from server.
                         Log.d("sunnyDay", "Closing the Socket...");
                         os.close();
                         is.close();
@@ -135,25 +145,24 @@ public class MainActivity extends AppCompatActivity {
 
         }.start();
 
-        updateTextView2(textViewMessage);
+//        updateTextView2(textViewMessage);
     }
 
     public static void receiveResultFromServer() {
         new Thread() {
             public void run() {
+
+                // initailize a Socket. Here Android device is serve and PC is client.
                 ServerSocket echoServer = null;
                 String line;
                 // DataInputStream is;
                 BufferedReader is;
                 PrintStream os;
                 Socket clientSocket = null;
-                // Try to open a server socket on port 9999
-                // Note that we can't choose a port less than 1023 if we are not
-                // privileged users (root)
 
                 Log.d("sunnyDay", "Initializing Socket...");
                 try {
-                    echoServer = new ServerSocket(9930);
+                    echoServer = new ServerSocket(PORT);
                 } catch (IOException e) {
                     System.out.println(e);
                 }
@@ -164,16 +173,16 @@ public class MainActivity extends AppCompatActivity {
                     clientSocket = echoServer.accept();
                     // is = new DataInputStream(clientSocket.getInputStream());
                     is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    os = new PrintStream(clientSocket.getOutputStream());
-                    // As long as we receive data, echo that data back to the client.
+//                    os = new PrintStream(clientSocket.getOutputStream());
 
                     Log.d("sunnyDay", "Connection with client established. Listening for incoming messages...");
                     while (true) {
                         line = is.readLine();
                         // os.println(line);
-                        Log.d("sunnyDay", "Echoed the message from client.");
+                        // Log.d("sunnyDay", "Echoed the message from client.");
 
                         result = line; // received the String from Android for temperature data
+                        Log.d("sunnyDay", result);
                         Log.d("sunnyDay", "Received analysis result. Closing server...");
                         break;
                     }
@@ -181,6 +190,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("sunnyDay", e.getMessage());
                 }
 
+                // After receiving the analysis result from PC, close the serverSocket.
                 Log.d("sunnyDay", "Closing sockets...");
                 try {
                     clientSocket.close();
