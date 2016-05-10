e4bbeb08138939f656793c4edbdaae51dd04f358
Yingbo (Max) Wang <maxwyb@gmail.com>
2016-05-09 21:33:02 -0700

    Create new Android Studio project.

---
+++ AndroidData/app/src/main/java/com/ucla/max/androiddata/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ucla.max.androiddata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void runSimulation(View view) {
+        Log.d("sunnyDay", "Run button clicked.");
+    }
+}
